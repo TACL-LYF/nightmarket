10818bc20fbd58c9c385a21e5a023fee6ab1e494
Rosette Su <su.rosette@gmail.com>
2019-01-02 16:14:42 -0800

    Add missing required field options on models

--- bahtzang/models.py
+++ bahtzang/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 
-
-# TODO: FOREIGN KEYS TO BE UPDATED
-# add_foreign_key "campers", "campers", column: "possible_dupe_of_id"
-# add_foreign_key "campers", "families"
-# add_foreign_key "referrals", "families"
-# add_foreign_key "referrals", "referral_methods"
-# add_foreign_key "registration_discounts", "camps"
-# add_foreign_key "registration_discounts", "registration_payments"
-# add_foreign_key "registrations", "campers"
-# add_foreign_key "registrations", "camps"
-# add_foreign_key "registrations", "registration_payments"
-
 class Camp(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     year = models.IntegerField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
@@ -26,10 +14,11 @@ class Camp(models.Model):
     registration_close_date = models.DateTimeField()
     camp_start_date = models.DateTimeField()
     camp_end_date = models.DateTimeField()
-    campsite = models.CharField()
-    campsite_address = models.CharField()
+    campsite = models.CharField(max_length=100)
+    campsite_address = models.CharField(max_length=100)
 
     class Meta:
+        managed = False
         ordering = ['year']
 
         def __unicode__(self):
@@ -37,23 +26,24 @@ class Camp(models.Model):
 
 
 class Family(models.Model):
-    primary_parent_first_name = models.CharField()
-    primary_parent_last_name = models.CharField()
-    primary_parent_email = models.CharField()
-    primary_parent_phone_number = models.CharField()
-    secondary_parent_first_name = models.CharField()
-    secondary_parent_last_name = models.CharField()
-    secondary_parent_email = models.CharField()
-    secondary_parent_phone_number = models.CharField()
-    suite = models.CharField()
-    street = models.CharField()
-    city = models.CharField()
-    state = models.CharField()
-    zip = models.CharField()
+    primary_parent_first_name = models.CharField(max_length=50)
+    primary_parent_last_name = models.CharField(max_length=50)
+    primary_parent_email = models.CharField(max_length=254)
+    primary_parent_phone_number = models.CharField(max_length=10)
+    secondary_parent_first_name = models.CharField(max_length=50)
+    secondary_parent_last_name = models.CharField(max_length=50)
+    secondary_parent_email = models.CharField(max_length=254)
+    secondary_parent_phone_number = models.CharField(max_length=10)
+    suite = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=9)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
     class Meta:
+        managed = False
         ordering = ['primary_parent_first_name', 'primary_parent_last_name']
 
         def __unicode__(self):
@@ -61,11 +51,11 @@ class Family(models.Model):
 
 
 class Camper(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     birthdate = models.DateTimeField()
     gender = models.IntegerField()
-    email = models.CharField()
+    email = models.CharField(max_length=254)
     medical_conditions_and_medication = models.TextField()
     diet_and_food_allergies = models.TextField()
     status = models.IntegerField(default=0)
@@ -76,36 +66,8 @@ class Camper(models.Model):
     possible_dupe_of_id = models.IntegerField()
 
     class Meta:
+        managed = False
         ordering = ['first_name', 'last_name']
-
-        def __unicode__(self):
-            return self.title
-
-
-class Registration(models.Model):
-    grade = models.IntegerField()
-    shirt_size = models.CharField()
-    bus = models.BooleanField()
-    additional_notes = models.TextField()
-    waiver_signature = models.CharField()
-    group = models.IntegerField()
-    camp_family = models.CharField()
-    cabin = models.CharField()
-    city = models.CharField()
-    state = models.CharField()
-    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
-    camper = models.ForeignKey(Camper, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    additional_shirts = models.TextField()
-    registration_payment_id = models.IntegerField()
-    camper_involvement = models.TextField()
-    jtasa_chapter = models.CharField()
-    preregistration = models.BooleanField(default=False)
-    status = models.IntegerField(default=0)
-
-    class Meta:
-        ordering = ['created_on']
 
         def __unicode__(self):
             return self.title
@@ -114,29 +76,64 @@ class Registration(models.Model):
 class Registration_Payment(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2)
     additional_donation = models.DecimalField(max_digits=10, decimal_places=2)
-    discount_code = models.CharField()
-    stripe_charge_id = models.CharField()
+    discount_code = models.CharField(max_length=50)
+    stripe_charge_id = models.CharField(max_length=50)
     breakdown = models.TextField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-    stripe_brand = models.CharField()
-    stripe_last_four = models.IntegerField()
+    stripe_brand = models.CharField(max_length=50)
+    stripe_last_four = models.CharField(max_length=4)
 
     class Meta:
-        ordering = ['stripe_charge_id']
+        managed = False
+
+        def __unicode__(self):
+            return self.title
+
+
+class Registration(models.Model):
+    grade = models.IntegerField()
+    shirt_size = models.CharField(max_length=50)
+    bus = models.BooleanField()
+    additional_notes = models.TextField()
+    waiver_signature = models.CharField(max_length=100)
+    group = models.IntegerField()
+    camp_family = models.CharField(max_length=100)
+    cabin = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
+    camper = models.ForeignKey(Camper, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    additional_shirts = models.TextField()
+    registration_payment = models.ForeignKey(Registration_Payment, on_delete=models.CASCADE)
+    camper_involvement = models.TextField()
+    jtasa_chapter = models.CharField(max_length=50)
+    preregistration = models.BooleanField(default=False)
+    status = models.IntegerField(default=0)
+
+    class Meta:
+        managed = False
 
         def __unicode__(self):
             return self.title
 
 
 class Registration_Discount(models.Model):
-    code = models.CharField()
+    code = models.CharField(max_length=50)
     discount_percent = models.IntegerField()
     redeemed = models.BooleanField(default=False)
-    camp = models.IntegerField()
-    registration_payment_id = models.IntegerField()
+    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
+    registration_payment = models.ForeignKey(Registration_Payment, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+
+        def __unicode__(self):
+            return self.title
 
 
 class Registration_Session(models.Model):
@@ -144,54 +141,84 @@ class Registration_Session(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    class Meta:
+        managed = False
+
+        def __unicode__(self):
+            return self.title
+
 
 class Last_Day_Purchase(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    email = models.CharField()
-    phone = models.CharField()
-    address = models.CharField()
-    city = models.CharField()
-    state = models.CharField()
-    zip = models.CharField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=254)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=9)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     company_match = models.BooleanField()
-    company = models.CharField()
-    stripe_charge_id = models.CharField()
-    stripe_brand = models.CharField()
-    stripe_last_four = models.CharField()
-    camper_names = models.CharField()
+    company = models.CharField(max_length=100)
+    stripe_charge_id = models.CharField(max_length=50)
+    stripe_brand = models.CharField(max_length=50)
+    stripe_last_four = models.CharField(max_length=4)
+    camper_names = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+
+        def __unicode__(self):
+            return self.title
 
 
 class Donation(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    email = models.CharField()
-    phone = models.CharField()
-    address = models.CharField()
-    city = models.CharField()
-    state = models.CharField()
-    zip = models.CharField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=254)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=9)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     company_match = models.BooleanField()
-    company = models.CharField()
-    stripe_charge_id = models.CharField()
-    stripe_brand = models.CharField()
-    stripe_last_four = models.CharField()
+    company = models.CharField(max_length=100)
+    stripe_charge_id = models.CharField(max_length=50)
+    stripe_brand = models.CharField(max_length=50)
+    stripe_last_four = models.CharField(max_length=4)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    class Meta:
+        managed = False
+
+        def __unicode__(self):
+            return self.title
+
 
 class Referral_Method(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     allow_details = models.BooleanField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    class Meta:
+        managed = False
+
+        def __unicode__(self):
+            return self.title
+
 
 class Referral(models.Model):
-    details = models.CharField()
-    family = models.ForeignKey(Family, on_delete=CASCADE)
+    details = models.CharField(max_length=100)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-    referral_method_id = family = models.ForeignKey(Referral_Method, on_delete=CASCADE)
+    referral_method = models.ForeignKey(Referral_Method, on_delete=models.CASCADE)
+
+    class Meta:
+        managed = False
+
+        def __unicode__(self):
+            return self.title
