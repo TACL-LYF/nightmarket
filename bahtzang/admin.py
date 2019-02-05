d0b63061ae03fa28f0e24d1dab448c2795fdac39
Rosette Su <su.rosette@gmail.com>
2019-02-05 15:34:17 -0800

    Update admin dashboard for registration-related models

--- bahtzang/admin.py
+++ bahtzang/admin.py
@@ -1,5 +1,262 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html, format_html_join
 
-from .models import Camp, Family
+from .models import *
 
-admin.site.register([Camp, Family])
+class ReferralInline(admin.TabularInline):
+    model = Referral
+    extra = 0
+    exclude = ('created_at', 'updated_at',)
+    readonly_fields = ['referral_method', 'details']
+    show_change_link = True
+
+class CamperInline(admin.TabularInline):
+    model = Camper
+    extra = 0
+    fields = ['first_name', 'last_name', 'gender', 'birthdate', 'status', 'get_registration_links_list']
+    readonly_fields = ['get_registration_links_list']
+    show_change_link = True
+
+class RegistrationInline(admin.TabularInline):
+    model = Registration
+    extra = 0
+    fields = ['camp', 'grade', 'shirt_size', 'bus', 'preregistration',]
+    show_change_link = True
+
+class CampAdmin(admin.ModelAdmin):
+    def registration_count(self, obj):
+        return obj.registration_set.count()
+    registration_count.short_description = "Registrations"
+
+    search_fields = ('name',)
+
+    list_display = ('year', 'name', 'registration_count')
+    list_display_links = ('year', 'name')
+
+    fieldsets = [
+        (None, {'fields': [
+            'year', 'name', 'campsite', 'campsite_address', 'camp_start_date',
+            'camp_end_date',
+        ]}),
+        ('Registration Details', {'fields': [
+            'registration_open_date', 'registration_late_date',
+            'registration_close_date', 'registration_fee', 'registration_late_fee',
+            'shirt_price', 'sibling_discount', 'waitlist_starts_after',
+        ]})
+    ]
+
+class FamilyAdmin(admin.ModelAdmin):
+    def campers(self, obj):
+        return format_html_join(
+            ', ', '<a href="{}">{}</a>',
+            ((reverse('admin:bahtzang_camper_change', args=(c.id,)), str(c)) for c in obj.camper_set.all())
+        )
+
+    search_fields = ('primary_parent_first_name', 'primary_parent_last_name',
+        'primary_parent_email', 'primary_parent_phone_number', 'city',
+        'secondary_parent_first_name', 'secondary_parent_last_name',
+        'secondary_parent_email', 'secondary_parent_phone_number', 'street')
+
+    list_display = ('primary_parent_first_name', 'primary_parent_last_name',
+        'primary_parent_email', 'primary_parent_phone_number', 'city', 'campers')
+    list_display_links = ('primary_parent_first_name', 'primary_parent_last_name')
+
+    fieldsets = [
+        ('Primary Parent Information', {'fields': [
+            ('primary_parent_first_name', 'primary_parent_last_name',),
+            ('primary_parent_email', 'primary_parent_phone_number',),
+        ]}),
+        ('Secondary Parent Information', {'fields': [
+            ('secondary_parent_first_name', 'secondary_parent_last_name',),
+            ('secondary_parent_email', 'secondary_parent_phone_number',),
+        ]}),
+        ('Address Information', {'fields': [
+            ('street', 'suite',),
+            ('city', 'state', 'zip'),
+        ]}),
+    ]
+    inlines = [ReferralInline, CamperInline]
+
+class CamperAdmin(admin.ModelAdmin):
+    def link_to_family(self, obj):
+        f = obj.family
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse('admin:bahtzang_family_change', args=(f.id,)),
+            str(f)
+        )
+
+    search_fields = ('first_name', 'last_name',
+        'medical_conditions_and_medication', 'diet_and_food_allergies')
+
+    list_display = ('first_name', 'last_name', 'gender', 'birthdate', 'email',
+        'link_to_family', 'get_registration_links_list', 'status')
+    list_display_links = ('first_name', 'last_name')
+    list_filter = ('status', 'gender', 'returning', 'registration__camp__year')
+
+    fields = [('status', 'returning',), 'first_name', 'last_name', 'birthdate',
+        'gender', 'email', 'medical_conditions_and_medication',
+        'diet_and_food_allergies']
+    inlines = [RegistrationInline]
+
+class RegistrationAdmin(admin.ModelAdmin):
+    def link_to_camper(self, obj):
+        c = obj.camper
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse('admin:bahtzang_camper_change', args=(c.id,)),
+            str(c)
+        )
+    link_to_camper.short_description = 'Camper'
+    link_to_camper.admin_order_field = 'camper'
+
+    def link_to_family(self, obj):
+        f = obj.camper.family
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse('admin:bahtzang_camper_change', args=(f.id,)),
+            str(f)
+        )
+    link_to_family.short_description = 'Parent'
+    link_to_family.admin_order_field = 'family'
+
+    def get_gender(self, obj):
+        return obj.camper.gender
+    get_gender.short_description = 'Gender'
+
+    def get_medical(self, obj):
+        return obj.camper.medical_conditions_and_medication
+    get_medical.short_description = 'Medical'
+
+    def get_allergies(self, obj):
+        return obj.camper.diet_and_food_allergies
+    get_allergies.short_description = 'Diet/Allergies'
+
+    search_fields = ('camper__first_name', 'camper__last_name')
+
+    list_display = ('created_at', 'link_to_camper', 'link_to_family',
+        'grade', 'get_gender', 'shirt_size', 'additional_shirts', 'bus',
+        'jtasa_chapter', 'get_medical', 'get_allergies', 'additional_notes',
+        'status')
+    list_filter = ('camp__year', 'status', 'preregistration')
+
+    fields = [('status', 'preregistration'), ('camper', 'camp'), ('grade', 'bus'),
+        'shirt_size', 'additional_shirts', 'jtasa_chapter', 'camper_involvement',
+        'additional_notes', 'registration_payment', ('created_at', 'updated_at'),
+        ('group','camp_family', 'cabin')
+    ]
+    readonly_fields = 'registration_payment', 'created_at', 'updated_at'
+
+class RegistrationPaymentRegistrationInline(admin.TabularInline):
+    model = Registration
+    extra = 0
+    readonly_fields = fields = ['camp', 'camper', 'additional_shirts']
+    can_delete = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+class RegistrationPaymentAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    list_display = ('created_at', 'get_registration_links_list', 'discount_code',
+        'additional_donation', 'total', 'stripe_brand', 'get_stripe_link')
+
+    inlines = [RegistrationPaymentRegistrationInline]
+
+    readonly_fields = fields = ('discount_code', 'additional_donation', 'total',
+        'stripe_brand', 'stripe_last_four', 'get_stripe_link', 'breakdown',
+        'created_at', 'updated_at', )
+
+class RegistrationDiscountAdmin(admin.ModelAdmin):
+    def link_to_registration_payment(self, obj):
+        rp = obj.registration_payment
+        if rp == None:
+            return rp
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse('admin:bahtzang_registration_payment_change', args=(rp.id,)),
+            str(rp)
+        )
+    link_to_registration_payment.short_description = 'Registration Payment'
+
+    list_display = ('code', 'camp', 'discount_percent', 'redeemed',
+        'link_to_registration_payment')
+    list_filter = ('camp', 'redeemed')
+
+    readonly_fields = ('redeemed', 'registration_payment', 'created_at',
+        'updated_at')
+
+class ReferralMethodAdmin(admin.ModelAdmin):
+    def referral_count(self, obj):
+        return obj.referral_set.count()
+    referral_count.short_description = "Referrals"
+
+    list_display = ('name', 'allow_details', 'referral_count')
+    list_filter = ('allow_details',)
+
+    fields = (('name', 'allow_details'), 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+class ReferralAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def link_to_family(self, obj):
+        f = obj.family
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse('admin:bahtzang_camper_change', args=(f.id,)),
+            str(f)
+        )
+    link_to_family.short_description = 'Family'
+    link_to_family.admin_order_field = 'family'
+
+    search_fields = ('details',)
+    list_display = ('link_to_family', 'referral_method', 'details', 'created_at')
+    list_filter = ('referral_method',)
+    list_display_links = None
+
+class DonationAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    search_fields = ('first_name', 'last_name', 'company')
+    list_display = ('created_at', 'first_name', 'last_name', 'email', 'phone',
+        'amount', 'company_match', 'company', 'get_stripe_link')
+    readonly_fields = ('first_name', 'last_name', 'email', 'phone', 'address',
+        'city', 'state', 'zip', 'amount', 'company_match', 'company',
+        'get_stripe_link', 'stripe_brand', 'stripe_last_four', 'created_at',
+        'updated_at')
+
+class LastDayPurchaseAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    search_fields = ('first_name', 'last_name', 'camper_names')
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'amount',
+        'camper_names', 'get_stripe_link')
+    fields = readonly_fields = ('first_name', 'last_name', 'camper_names', 'email',
+        'phone', 'address', 'city', 'state', 'zip', 'amount', 'dollar_for_dollar',
+        'company', 'get_stripe_link', 'stripe_brand', 'stripe_last_four',)
+
+
+admin.site.register(Camp, CampAdmin)
+admin.site.register(Family, FamilyAdmin)
+admin.site.register(Camper, CamperAdmin)
+admin.site.register(Registration, RegistrationAdmin)
+admin.site.register(Registration_Payment, RegistrationPaymentAdmin)
+admin.site.register(Registration_Discount, RegistrationDiscountAdmin)
+admin.site.register(Referral_Method, ReferralMethodAdmin)
+admin.site.register(Referral, ReferralAdmin)
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(Last_Day_Purchase, LastDayPurchaseAdmin)
--- bahtzang/models.py
+++ bahtzang/models.py
@@ -1,27 +1,33 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.html import format_html, format_html_join
 
 class Camp(models.Model):
-    name = models.CharField(max_length=100)
     year = models.IntegerField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    campsite = models.CharField(max_length=100)
+    campsite_address = models.CharField(max_length=100)
+    camp_start_date = models.DateField()
+    camp_end_date = models.DateField()
+    registration_open_date = models.DateField()
+    registration_late_date = models.DateField()
+    registration_close_date = models.DateField()
     registration_fee = models.DecimalField(max_digits=10, decimal_places=2)
     shirt_price = models.DecimalField(max_digits=6, decimal_places=2)
     sibling_discount = models.DecimalField(max_digits=6, decimal_places=2)
     registration_late_fee = models.DecimalField(max_digits=6, decimal_places=2)
-    registration_open_date = models.DateTimeField()
-    registration_late_date = models.DateTimeField()
-    registration_close_date = models.DateTimeField()
-    camp_start_date = models.DateTimeField()
-    camp_end_date = models.DateTimeField()
-    campsite = models.CharField(max_length=100)
-    campsite_address = models.CharField(max_length=100)
+    waitlist_starts_after = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    @property
+    def full_name(self):
+        return '%s: %s' % (self.year, self.name)
 
     def __str__(self):
-        return f"{self.year}: {self.name}"
+        return self.full_name
 
     class Meta:
-        managed = False
         ordering = ['year']
 
         def __unicode__(self):
@@ -29,16 +35,16 @@ class Camp(models.Model):
 
 
 class Family(models.Model):
-    primary_parent_first_name = models.CharField(max_length=50)
-    primary_parent_last_name = models.CharField(max_length=50)
-    primary_parent_email = models.CharField(max_length=254)
-    primary_parent_phone_number = models.CharField(max_length=10)
-    secondary_parent_first_name = models.CharField(max_length=50)
-    secondary_parent_last_name = models.CharField(max_length=50)
-    secondary_parent_email = models.CharField(max_length=254)
-    secondary_parent_phone_number = models.CharField(max_length=10)
-    suite = models.CharField(max_length=50)
+    primary_parent_first_name = models.CharField('First Name', max_length=50)
+    primary_parent_last_name = models.CharField('Last Name', max_length=50)
+    primary_parent_email = models.EmailField('Email', max_length=254)
+    primary_parent_phone_number = models.CharField('Phone Number', max_length=10)
+    secondary_parent_first_name = models.CharField('First Name', max_length=50)
+    secondary_parent_last_name = models.CharField('Last Name', max_length=50)
+    secondary_parent_email = models.EmailField('Email', max_length=254)
+    secondary_parent_phone_number = models.CharField('Phone Number', max_length=10)
     street = models.CharField(max_length=50)
+    suite = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
     zip = models.CharField(max_length=9)
@@ -46,10 +52,9 @@ class Family(models.Model):
     updated_at = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.primary_parent_first_name} {self.primary_parent_last_name}"
+        return '%s %s' % (self.primary_parent_first_name, self.primary_parent_last_name)
 
     class Meta:
-        managed = False
         verbose_name_plural = "families"
         ordering = ['primary_parent_first_name', 'primary_parent_last_name']
 
@@ -66,23 +71,34 @@ class Camper(models.Model):
         (0, 'Active'),
         (1, 'Graduated'),
     )
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    birthdate = models.DateTimeField()
+    birthdate = models.DateField()
     gender = models.IntegerField(choices=GENDER_CHOICES)
-    email = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
     medical_conditions_and_medication = models.TextField()
     diet_and_food_allergies = models.TextField()
+    returning = models.BooleanField()
     status = models.IntegerField(default=0, choices=STATUS_CHOICES)
-    family = models.ForeignKey(Family, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-    returning = models.BooleanField()
-    possible_dupe_of_id = models.IntegerField()
+
+    def get_registration_links_list(self):
+        return format_html_join(
+                ', ', '<a href="{}">{}</a>',
+                ((reverse('admin:bahtzang_registration_change', args=(r.id,)),
+                    r.camp.year) for r in self.registration_set.all())
+            )
+    get_registration_links_list.short_description = 'Registrations'
+
+    @property
+    def full_name(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
 
     def __str__(self):
-        managed = False
-        return f"{self.first_name} {self.last_name}"
+        return self.full_name
 
     class Meta:
         ordering = ['first_name', 'last_name']
@@ -92,18 +108,36 @@ class Camper(models.Model):
 
 
 class Registration_Payment(models.Model):
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    breakdown = models.TextField()
     additional_donation = models.DecimalField(max_digits=10, decimal_places=2)
     discount_code = models.CharField(max_length=50)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
     stripe_charge_id = models.CharField(max_length=50)
-    breakdown = models.TextField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
     stripe_brand = models.CharField(max_length=50)
     stripe_last_four = models.CharField(max_length=4)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def get_registration_links_list(self):
+        return format_html_join(
+            ', ', '<a href="{}">{} ({})</a>',
+            ((reverse('admin:bahtzang_registration_change', args=(r.id,)),
+                r.camper.full_name, r.camp.year) for r in self.registration_set.all())
+        )
+    get_registration_links_list.short_description = 'Registrations'
+
+    def get_stripe_link(self):
+        return format_html(
+            '<a href="https://dashboard.stripe.com/payments/{}" target="_blank">Open in Stripe</a>',
+            self.stripe_charge_id,
+        )
+    get_stripe_link.short_description = 'Stripe Link'
+
+    def __str__(self):
+        return 'RP#%d' % (self.id)
 
     class Meta:
-        managed = False
+        verbose_name = 'Registration Payment'
 
         def __unicode__(self):
             return self.title
@@ -111,55 +145,55 @@ class Registration_Payment(models.Model):
 
 class Registration(models.Model):
     SHIRT_SIZES = (
-        ('XS', 'X-Small'),
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'X-Large'),
-        ('2XL', 'XX-Large'),
+        (0, 'X-Small'),
+        (1, 'Small'),
+        (2, 'Medium'),
+        (3, 'Large'),
+        (4, 'X-Large'),
+        (5, 'XX-Large'),
     )
     STATUS_CHOICES = (
         (0, 'Active'),
         (1, 'Cancelled'),
         (2, 'Waitlisted'),
     )
+    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
+    camper = models.ForeignKey(Camper, on_delete=models.CASCADE)
+    registration_payment = models.ForeignKey(Registration_Payment, on_delete=models.CASCADE)
     grade = models.IntegerField()
     shirt_size = models.CharField(max_length=2, choices=SHIRT_SIZES)
+    additional_shirts = models.TextField()
     bus = models.BooleanField()
+    jtasa_chapter = models.CharField(max_length=50)
+    camper_involvement = models.TextField()
     additional_notes = models.TextField()
     waiver_signature = models.CharField(max_length=100)
+    waiver_date = models.DateField()
+    preregistration = models.BooleanField(default=False)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
     group = models.IntegerField()
     camp_family = models.CharField(max_length=100)
     cabin = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
-    camper = models.ForeignKey(Camper, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    additional_shirts = models.TextField()
-    registration_payment = models.ForeignKey(Registration_Payment, on_delete=models.CASCADE)
-    camper_involvement = models.TextField()
-    jtasa_chapter = models.CharField(max_length=50)
-    preregistration = models.BooleanField(default=False)
     status = models.IntegerField(default=0, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField('Registered')
+    updated_at = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.camper} ({self.camp.year})"
+        return '%s (%d)' % (str(self.camper), self.camp.year)
 
     class Meta:
-        managed = False
 
         def __unicode__(self):
             return self.title
 
 
 class Registration_Discount(models.Model):
+    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
+    registration_payment = models.ForeignKey(Registration_Payment, on_delete=models.CASCADE)
     code = models.CharField(max_length=50)
     discount_percent = models.IntegerField()
     redeemed = models.BooleanField(default=False)
-    camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
-    registration_payment = models.ForeignKey(Registration_Payment, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
@@ -167,9 +201,9 @@ class Registration_Discount(models.Model):
         return self.code
 
     class Meta:
-        managed = False
+        verbose_name = 'Registration Discount'
 
-        def __str__(self):
+        def __unicode__(self):
             return self.title
 
 
@@ -179,7 +213,6 @@ class Registration_Session(models.Model):
     updated_at = models.DateTimeField()
 
     class Meta:
-        managed = False
 
         def __unicode__(self):
             return self.title
@@ -188,22 +221,34 @@ class Registration_Session(models.Model):
 class Last_Day_Purchase(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=10)
+    camper_names = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
     zip = models.CharField(max_length=9)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    company_match = models.BooleanField()
+    dollar_for_dollar = models.BooleanField()
     company = models.CharField(max_length=100)
     stripe_charge_id = models.CharField(max_length=50)
     stripe_brand = models.CharField(max_length=50)
     stripe_last_four = models.CharField(max_length=4)
-    camper_names = models.CharField(max_length=100)
+    # created_at = models.DateTimeField()
+    # updated_at = models.DateTimeField()
+
+    def get_stripe_link(self):
+        return format_html(
+            '<a href="https://dashboard.stripe.com/payments/{}" target="_blank">Open in Stripe</a>',
+            self.stripe_charge_id,
+        )
+    get_stripe_link.short_description = 'Stripe Link'
+
+    def __str__(self):
+        return f"#{self.id} - {self.email}"
 
     class Meta:
-        managed = False
+        verbose_name = 'Last Day Purchase'
 
         def __unicode__(self):
             return self.title
@@ -212,7 +257,7 @@ class Last_Day_Purchase(models.Model):
 class Donation(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=10)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
@@ -227,8 +272,17 @@ class Donation(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    def get_stripe_link(self):
+        return format_html(
+            '<a href="https://dashboard.stripe.com/payments/{}" target="_blank">Open in Stripe</a>',
+            self.stripe_charge_id,
+        )
+    get_stripe_link.short_description = 'Stripe Link'
+
+    def __str__(self):
+        return f"Donation from {self.first_name} {self.last_name}"
+
     class Meta:
-        managed = False
 
         def __unicode__(self):
             return self.title
@@ -244,24 +298,23 @@ class Referral_Method(models.Model):
         return self.name
 
     class Meta:
-        managed = False
+        verbose_name = 'Referral Method'
 
         def __unicode__(self):
             return self.title
 
 
 class Referral(models.Model):
-    details = models.CharField(max_length=100)
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    referral_method = models.ForeignKey(Referral_Method, on_delete=models.CASCADE)
+    details = models.CharField(max_length=100)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-    referral_method = models.ForeignKey(Referral_Method, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.family} - {self.referral_method}"
 
     class Meta:
-        managed = False
 
         def __unicode__(self):
             return self.title
